e2983318b1cf9af81b551d915d972391671ed5de
taetae <taetae.dev@gmail.com>
2020-12-03 15:43:15 +0900

    add: @IBoutlet, ImagePicker for storyboard
    회원가입 첫화면에 필요한 아웃렛, ImagePicker  변수 추가

--- SignUpFlow/SignUpFlow/RegisterStepOneViewController.swift
+++ SignUpFlow/SignUpFlow/RegisterStepOneViewController.swift
@@ -7,8 +7,17 @@
 
 import UIKit
 
-class RegisterStepOneViewController: UIViewController {
-
+class RegisterStepOneViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    
+    let profileImagePicker = UIImagePickerController()
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var checkPasswordTextField: UITextField!
+    @IBOutlet weak var introductionTextView: UITextView!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
---
+++ SignUpFlow/SignUpFlow/RegisterStepTwoViewController.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterSetpTwoViewController.swift
+//  SignUpFlow
+//
+//  Created by 김태형 on 2020/12/03.
+//
+
+import UIKit
